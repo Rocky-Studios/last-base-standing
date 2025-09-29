43db938b339ec1e7f5d5698275f1026aa47d4c06
Gergo Kaszas <gergo.kaszas@outlook.com>
2025-09-29 13:07:53 +1000

    Move hardening code into hardening handler

--- src/main/kotlin/net/rockystudios/lastbasestanding/LastBaseStanding.kt
+++ src/main/kotlin/net/rockystudios/lastbasestanding/LastBaseStanding.kt
@@ -11,32 +11,8 @@ import net.minecraft.util.ActionResult
 import net.minecraft.util.Hand
 
 object LastBaseStanding : ModInitializer {
-    private val logger = LoggerFactory.getLogger("last-base-standing")
 
 	override fun onInitialize() {
-		UseBlockCallback.EVENT.register(UseBlockCallback { player, world, hand, hitResult ->
-			if (!world.isClient && player.getStackInHand(hand).item == Items.DIAMOND) {
-				val pos = hitResult.blockPos
-				HardeningHandler.harden(pos)
-				player.sendMessage(Text.of("Block hardened!"), true)
-
-				// Spawn particles at the block position (server-side)
-				if (world is ServerWorld) {
-					for (i in 0 until 20) {
-						val offsetX = world.random.nextDouble()
-						val offsetY = world.random.nextDouble()
-						val offsetZ = world.random.nextDouble()
-						world.spawnParticles(
-							ParticleTypes.END_ROD,
-							pos.x + offsetX, pos.y + offsetY, pos.z + offsetZ,
-							1, 0.0, 0.0, 0.0, 0.0
-						)
-					}
-				}
-				ActionResult.SUCCESS
-			} else {
-				ActionResult.PASS
-			}
-		})
+		HardeningHandler.initHardening()
 	}
 }
