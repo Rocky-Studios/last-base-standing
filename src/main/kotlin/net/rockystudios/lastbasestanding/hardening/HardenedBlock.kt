1a6717e0a898592fba75bf7d20c7b66eac3a8f0c
Gergo Kaszas <gergo.kaszas@outlook.com>
2025-09-29 18:11:29 +1000

    Fix name of HardenedBlock.kt

--- src/main/kotlin/net/rockystudios/lastbasestanding/hardening/HardenedBlock.kt
+++ src/main/kotlin/net/rockystudios/lastbasestanding/hardening/HardenedBlock.kt
@@ -1,6 +1,5 @@
 package net.rockystudios.lastbasestanding.hardening
 
-import net.minecraft.item.Item
 import net.minecraft.util.math.BlockPos
 
 class HardenedBlock(val position: BlockPos, val material: HardeningMaterial) {
